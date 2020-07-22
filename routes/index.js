903bf3411646f202c16eefb3bb676a76f47f0796
Luiz Moura <luizmoura2@hotmail.com>
2020-07-21 23:00:41 -0300

    166_MY05 - Displaying database data with EJS Embedded JavaScript 167_MY06 - EJS Include 168_MY07 - Organizing HEADER with includes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
+const conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, result)=>{
+    if (err){
+     console.log(err);
+    }
+    res.render('index', { 
+      title: 'Restaurante Saboroso!',
+      menus: result
+    });
+ });  
 });
 
+router.get('/contacts', function(req, res, next){
+  res.render('contacts', { 
+    title: 'Contatos - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'  
+  });
+    
+});
+
+router.get('/menus', function(req, res, next){
+  res.render('menus',  { title: 'Menus -Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!!'  
+  });
+});
+
+router.get('/reservations', function(req, res, next){
+  res.render('reservations', { title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'  
+  });
+});
+
+router.get('/services', function(req, res, next){
+  res.render('services', { title: 'Serviços - Menus -Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'É um prazer poder servir!'
+    });
+});
 module.exports = router;
