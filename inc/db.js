5ac3adecccc7d33bd89fa251af4f73ff09515845
Luiz Moura <luizmoura2@hotmail.com>
2020-07-21 21:19:46 -0300

    165_MY04 - Connecting NodeJS with MySql

---
+++ inc/db.js
@@ -0,0 +1,35 @@
+
+// get the client
+const mysql = require('mysql2');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'saboroso',
+    password: '#luciart#'
+    
+});
+
+module.exports = connection;
+    
+/*
+// simple query
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+ 
+// with placeholder 
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Page', 45],
+  function(err, results) {
+    console.log(results);
+  }
+);
+Us
+    */
